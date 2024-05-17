5b65a78cdb339e560da3308252fdb4197c0f39f9
GicheruBenjamin <projectbuild201@gmail.com>
2024-05-17 21:59:45 +0300

    applicDone

---
+++ OpsJs/applic.js
@@ -0,0 +1,9 @@
+
+//Require ops
+const ops = require('./ops');
+
+//Show how it can be done
+console.log(ops.add(1,2));
+console.log(ops.subtract(2,1));
+console.log(ops.multiply(2,2));
+console.log(ops.division(4,2));
--- OpsJs/ops.js
+++ OpsJs/ops.js
@@ -1,20 +1,19 @@
 
 // An object containing methods for doing operations
-// operations.js
 
-const operations = {
-    plus: function(a, b) {
+const ops = {
+    add: function(a, b) {
       return a + b;
     },
-    minus: function(a, b) {
+    subtract: function(a, b) {
       return a - b;
     },
-    dot: function(a, b) {
+    multiply: function(a, b) {
       return a * b;
     },
-    star: function(a, b) {
+    division: function(a, b) {
       return a / b;
     }
   };
   
-  module.exports = operations;
+  module.exports = ops;
