07c3ce66669770f3418da7575fd91519ade40c7d
GicheruBenjamin <projectbuild201@gmail.com>
2024-05-17 21:53:20 +0300

    opDone>>

--- OpsJs/ops.js
+++ OpsJs/ops.js
@@ -1,2 +1,20 @@
 
 // An object containing methods for doing operations
+// operations.js
+
+const operations = {
+    plus: function(a, b) {
+      return a + b;
+    },
+    minus: function(a, b) {
+      return a - b;
+    },
+    dot: function(a, b) {
+      return a * b;
+    },
+    star: function(a, b) {
+      return a / b;
+    }
+  };
+  
+  module.exports = operations;
